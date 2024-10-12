bd2a52a98775f99ecb838657cf9ae984b9940b60
BirdiD <diioula.doucoure@gmail.com>
2024-10-12 15:18:44 +0200

    add expectations.py

--- src/fabric_data_guard/expectations.py
+++ src/fabric_data_guard/expectations.py
@@ -5,7 +5,6 @@ def create_expectation(expectation_type, **kwargs):
     """
     A simple wrapper to create Great Expectations expectation objects.
     This function exists to provide a consistent interface and potential for future extensions.
-
     Args:
         expectation_type (str): The type of expectation to create.
         **kwargs: Arguments to pass to the expectation constructor.
